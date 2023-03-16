3be6f7f994a04df3f4d278f8b03d255b8c241b55
Harley <74605804+harley-hutchinson@users.noreply.github.com>
2023-03-16 17:39:07 +0000

    more css & added mixin

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -5,20 +5,24 @@ export const LandingPage = () => {
     <>
       <main className="landing">
         <section className="landing__top">
-          <div>
-            <p>THE COMPLETE WALLET</p>
-            <p>
+          <div className="landing__top__main-info">
+            <h1>THE COMPLETE WALLET</h1>
+            <h2>
               Manage your <br />
               Finances
               <br />
               Effectively
-            </p>
+            </h2>
           </div>
         </section>
         <section className="landing__bottom">
           <div className="buttoms">
-            <a href="google.com">Sign up</a>
-            <a href="google.com">Login</a>
+            <a className="buttom" href="google.com">
+              Sign up
+            </a>
+            <a className="buttom" href="google.com">
+              Login
+            </a>
           </div>
         </section>
       </main>
