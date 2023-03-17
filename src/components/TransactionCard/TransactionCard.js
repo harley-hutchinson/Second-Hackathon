94a36ea91cd2407f7746fbf081713d8bbe5cfca8
jinsp43 <jinsp43@gmail.com>
2023-03-17 00:18:13 +0000

    pull data from api

--- src/components/TransactionCard/TransactionCard.js
+++ src/components/TransactionCard/TransactionCard.js
@@ -1,7 +1,14 @@
 import "./TransactionCard.scss";
 
-const TransactionCard = () => {
-  return <article></article>;
+const TransactionCard = ({ transaction }) => {
+  return (
+    <article className="transaction">
+      <h3 className="transaction__heading">{transaction.title}</h3>
+      <p className="transaction__category">{transaction.category}</p>
+      <p className="transaction__desc">{transaction.description}</p>
+      <h3 className="transaction__amount">{transaction.amount}</h3>
+    </article>
+  );
 };
 
 export default TransactionCard;
--- src/pages/Dashboard/DashboardAll.js
+++ src/pages/Dashboard/DashboardAll.js
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
+import TransactionCard from "../../components/TransactionCard/TransactionCard";
+import { GET_TRANSACTIONS } from "../../utils/apiCalls.mjs";
 import "./DashboardAll.scss";
 
 const DashboardAll = () => {
+  const [transactions, setTransactions] = useState();
+
+  useEffect(() => {
+    const getTransactions = async () => {
+      try {
+        const { data } = await GET_TRANSACTIONS();
+        console.log(data);
+        setTransactions(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getTransactions();
+  }, []);
+
+  if (!transactions) {
+    return <p>loading...</p>;
+  }
+
   return (
     <section className="all">
       <h2 className="all__heading">All Transactions</h2>
-      <div className="all__list"></div>
+      <div className="all__list">
+        {transactions.map((transaction) => {
+          return (
+            <TransactionCard key={transaction.id} transaction={transaction} />
+          );
+        })}
+      </div>
     </section>
   );
 };
