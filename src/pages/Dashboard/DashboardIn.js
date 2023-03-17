44d562bdab17fdf9953654e8bd042cd0d13555b7
jinsp43 <jinsp43@gmail.com>
2023-03-17 11:23:09 +0000

    get all in and out working

--- src/pages/Dashboard/DashboardIn.js
+++ src/pages/Dashboard/DashboardIn.js
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
+import TransactionCard from "../../components/TransactionCard/TransactionCard.js";
+import { GET_IN_TRANSACTIONS } from "../../utils/apiCalls.mjs";
 import "./DashboardIn.scss";
 
 const DashboardIn = () => {
+  const [transactions, setTransactions] = useState();
+
+  useEffect(() => {
+    const getTransactions = async () => {
+      try {
+        const { data } = await GET_IN_TRANSACTIONS();
+        setTransactions(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getTransactions();
+  }, []);
+
+  if (!transactions) {
+    return <p>loading...</p>;
+  }
+
   return (
     <section className="in">
       <h2 className="in__heading">In</h2>
-      <div className="in__list"></div>
+      <div className="in__list">
+        {transactions.map((transaction) => {
+          return (
+            <TransactionCard key={transaction.id} transaction={transaction} />
+          );
+        })}
+      </div>
     </section>
   );
 };
--- src/pages/Dashboard/DashboardOut.js
+++ src/pages/Dashboard/DashboardOut.js
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import TransactionCard from "../../components/TransactionCard/TransactionCard.js";
+import { GET_OUT_TRANSACTIONS } from "../../utils/apiCalls.mjs";
 import "./DashboardOut.scss";
 
 const DashboardOut = () => {
+  const [transactions, setTransactions] = useState();
+
+  useEffect(() => {
+    const getTransactions = async () => {
+      try {
+        const { data } = await GET_OUT_TRANSACTIONS();
+        setTransactions(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getTransactions();
+  }, []);
+
+  if (!transactions) {
+    return <p>loading...</p>;
+  }
   return (
     <section className="out">
       <h2 className="out__heading">Out</h2>
-      <div className="out__list"></div>
+      <div className="out__list">
+        {transactions.map((transaction) => {
+          return (
+            <TransactionCard key={transaction.id} transaction={transaction} />
+          );
+        })}
+      </div>
     </section>
   );
 };
